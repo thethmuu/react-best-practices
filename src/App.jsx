adb43e0203ee73c7739f33a3007faa2f08492dfa
Thet Hmuu Eain Soe <thethmuueinsoe@gmail.com>
2022-11-17 21:36:43 +0630

    setup with basic style

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import HomePage from './pages/Home';
-import SignInPage from './pages/SignIn';
-
-function App() {
+const App = () => {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/signin' element={<SignInPage />} />
-      </Routes>
-    </Router>
+    <section className='px-6 py-8 mx-auto bg-white border-gray-200 rounded shadow w-96'>
+      <h2 className='text-2xl font-semibold text-center'>Sign In</h2>
+      <form onSubmit={handleSubmit}>
+        <div className='w-full form-control'>
+          <label className='label'>Email</label>
+          <input
+            type='email'
+            placeholder='Type here'
+            className='w-full input input-bordered input-sm'
+          />
+        </div>
+        <div className='w-full form-control'>
+          <label className='label'>Password</label>
+          <input
+            type='password'
+            placeholder='Type here'
+            className='w-full input input-bordered input-sm'
+          />
+        </div>
+        <div className='flex justify-center'>
+          <button className='mt-5 btn btn-sm btn-primary'>Submit</button>
+        </div>
+      </form>
+    </section>
   );
-}
+};
 
 export default App;
